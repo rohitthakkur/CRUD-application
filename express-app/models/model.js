f664243a112af85ee4f33ac64ddbae1cc674c486
rohitthakkur <98377865+rohitthakkur@users.noreply.github.com>
2023-11-25 00:52:10 +0530

    Backend

--- express-app/models/model.js
+++ express-app/models/model.js
@@ -1,7 +1,3 @@
-//mongoose is module to connect express with mongo Db
-//Schema is the structure that we follow when enter data in the db 
-// s is added itself in the collection name
-
 const mongoose = require("mongoose");
 
 const dataSchema = new mongoose.Schema({
@@ -20,11 +16,3 @@ const dataSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model("Product",dataSchema);
-
-// localhost:8080/api/postData
-
-// {
-//     "name":"Kia",
-//     "brand":"Sonet",
-//     "quantity":"1000"
-// }
